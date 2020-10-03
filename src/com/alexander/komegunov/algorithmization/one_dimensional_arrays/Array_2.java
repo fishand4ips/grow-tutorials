f173cca96aeba66189e880780b50d904a73b0a0e
fishand4ips <alekomegunov@gmail.com>
2020-10-03 13:46:46 +0300

    solve one_dimen_2 tasks

---
+++ src/com/alexander/komegunov/algorithmization/one_dimensional_arrays/Array_2.java
@@ -0,0 +1,24 @@
+package com.alexander.komegunov.algorithmization.one_dimensional_arrays;
+
+import java.util.Arrays;
+
+public class Array_2 {
+
+    public static int compute(byte[] array) {
+        double z = Math.random() * 10;
+        int counter = 0;
+        System.out.println("Исходный массив: " + Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > z) {
+                array[i] = (byte) z;
+                counter++;
+            }
+        }
+        System.out.println("Новый массив: " + Arrays.toString(array));
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Количество замен: " + compute(new byte[]{1, 2, 13, 4, 5, 6, 7, 5, 3, 2, 24, 5, 4, 3, 2}));
+    }
+}
