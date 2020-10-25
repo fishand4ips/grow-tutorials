8fe376625cee1f49a924af8e4cc687fe93a8d6d2
fishand4ips <alekomegunov@gmail.com>
2020-10-25 19:39:45 +0300

    factorial

---
+++ src/com/alexander/komegunov/different/Factorial.java
@@ -0,0 +1,22 @@
+package com.alexander.komegunov.different;
+
+import javax.swing.*;
+
+public class Factorial {
+
+    /**
+     * Checking correct operation
+     * @param args
+     */
+    public static void main(String[] args) {
+        final String input = JOptionPane.showInputDialog("Введи положительное число ");
+        JOptionPane.showMessageDialog(null, "Факториал " + input + ": " + factorial(Integer.parseInt(input)));
+    }
+
+    static long factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("negative number");
+        }
+        return n > 1 ? n * factorial(n - 1) : 1;
+    }
+}
